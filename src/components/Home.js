fe5598fcae30bb3cbe91530cfe25fd89a499753a
Ela <teodocio.am@gmail.com>
2025-04-03 08:36:34 -0500

    feat: fix responsive styling for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,13 +9,13 @@ export default function Home() {
                     <h1>AENA</h1>
                     <h1>TEODOCIO</h1>
                 </div>
-                <img src={heroImg} alt="Photo by Lorenzo on Unsplash" id="heroImg" />
-            </div>
-            <div id={ABOUT[0].id}>
+                <img src={heroImg} alt="Hero by Lorenzo on Unsplash" id="heroImg" className="block" />
+                <div id={ABOUT[0].id}>
                 <h4>{ABOUT[0].title}</h4>
                 <p>
                     {ABOUT[0].text}
                 </p>
+            </div>
             </div>
         </>
     )
--- src/components/Work.js
+++ src/components/Work.js
@@ -9,16 +9,14 @@ export default function Work() {
             {WORKS.map(work => (
                 
                 <div key={work.id} class="card d-inline-block bg-dark text-white">
-                    <img class="card-img" src={work.imgSrc} alt="Card image" />
+                    <img class="card-img" src={work.imgSrc} alt="Card background" />
                     <div class="card-img-overlay d-flex flex-column justify-content-center">
                         <h4 class="card-title"><strong>{work.title}</strong></h4>
                         <p class="card-text">{work.description}</p>
                         <p class="card-text">Built with: <br />{work.techList}</p>
                         <a href={work.link} target="_blank" rel="noopener noreferrer" className="cardLink text-decoration-none btn text-white fs-2"><FaGithub /></a>
                     </div>
-                    
                 </div>
-                
             ))}
         </div>
     </div>
